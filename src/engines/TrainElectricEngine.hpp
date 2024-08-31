70fc64fdb59feadd2e7693115b0af921973b3998
DoS <karoltomaszewskimusic@gmail.com>
2024-08-31 16:52:22 +0200

    Add missing parts and Mover logic

--- src/engines/TrainElectricEngine.hpp
+++ src/engines/TrainElectricEngine.hpp
@@ -29,8 +29,9 @@ namespace godot {
             float csw = 0;
             /**
              * The minimum closing voltage of the main switch, in volts.
+             * By default, it is 0.5*max_voltage
              */
-            float min_main_switch_voltage = 0;
+            float min_main_switch_voltage = 0.5f*max_voltage;
             /**
              * Minimum pressure in the Pantograph Tank (PT) to raise the pantograph, in MPa
              */
@@ -39,6 +40,19 @@ namespace godot {
              * Maximum pressure in the Pantograph Tank
              */
             float max_pantograph_tank_pressure = 0;
+            /**
+             *Value that makes overvoltage relay
+             */
+            bool overvoltage_relay = false;
+            /**
+             * The voltage required to turn on the main switch
+             * By default, it is 0.6*max_voltage
+             */
+            float required_main_switch_voltage = 0.6f*max_voltage;
+            float transducer_input_voltage = 0;
+            int accumulator_recharge_source = 0;
+            int power_cable_power_trans = 0;
+            float power_cable_steam_pressure = 0;
             //@TODO: Implement bitmask for PhysicalLayout
         protected:
             void _do_update_internal_mover(TMoverParameters *mover) override;
@@ -68,9 +82,21 @@ namespace godot {
             float get_min_pantograph_tank_pressure() const;
             void set_max_pantograph_tank_pressure(float p_max_pantograph_tank_pressure);
             float get_max_pantograph_tank_pressure() const;
+            void set_overvoltage_relay_value(bool p_overvoltage_relay_value);
+            bool get_overvoltage_relay_value() const;
+            void set_required_main_switch_voltage(float p_required_main_switch_voltage);
+            float get_required_main_switch_voltage() const;
             void set_csw(float p_csw);
 
             TrainElectricEngine();
             ~TrainElectricEngine() override = default;
+            void set_transducer_input_voltage(const float p_required_main_switch_voltage);
+            float get_transducer_input_voltage() const;
+            void set_accumulator_recharge_source(int p_source);
+            int get_accumulator_recharge_source() const;
+            void set_power_cable_power_source(int p_source);
+            int get_power_cable_power_source() const;
+            void set_power_cable_steam_pressure(float p_pressure);
+            float get_power_cable_steam_pressure() const;
     };
 } // namespace godot

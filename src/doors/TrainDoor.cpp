bd38135194bd3e562f7e44f0a0554a0dfad8428e
DoS <karoltomaszewskimusic@gmail.com>
2024-09-05 10:16:57 +0200

    Add all door section properities into header file

---
+++ src/doors/TrainDoor.cpp
@@ -0,0 +1,9 @@
+#include <godot_cpp/classes/gd_extension.hpp>
+#include <godot_cpp/classes/node.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+
+#include "TrainDoor.hpp"
+
+namespace godot {
+    void TrainDieselEngine::_bind_methods() { }
+}

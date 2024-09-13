484359726e40a90db4aa27bffdbcfcd6bb50a29c
Marcin Nowak <marcin.j.nowak@gmail.com>
2024-09-13 03:06:09 +0200

    fixes4

--- demo/powered_train_part.gd
+++ demo/powered_train_part.gd
@@ -1,7 +1,7 @@
 extends GenericTrainPart
 class_name PoweredTrainPart
 
-## Example of a customization of based on GenericTrainPart interface.
+## Example of a customization based on a GenericTrainPart interface.
 ## PoweredTrainPart will call [method _process_powered] when low power is
 ## available in a train (24V or 110V), [method _process_unpowered] otherwise.
 
@@ -13,3 +13,13 @@ func _process_train_part(delta):
         call("_process_powered", delta)
     elif not power_avail and has_method("_process_unpowered"):
         call("_process_unpowered", delta)
+
+
+## Will be called when low power is available (24V or 110V)
+func _process_powered(delta):
+    pass
+
+
+## Will be called when low power is not available (no 24V nor 110V)
+func _process_unpowered(delta):
+    pass

14ff566c425fc0686a1d620a2a5b5101c2bd70c1
DoS <karoltomaszewskimusic@gmail.com>
2025-05-03 11:24:58 +0200

    Add dynamic audio crossfade and layered sound handling with complete LayeredSoundResource documentation

--- demo/sound_test/sound_test.gd
+++ demo/sound_test/sound_test.gd
@@ -1,35 +1,77 @@
 extends Control
-var _engine_audio_stream = false
+var engine_sounds: LayeredSoundResource = null
+@onready var player_1 = $AudioStreamPlayer3D
+@onready var player_2 = $AudioStreamPlayer3D2
+@onready var sounds = $TrainSound
+@export var MAX_VOLUME_DB: float = 0.0
+@export var MIN_VOLUME_DB = -80.0
 func _ready():
-    var sounds = $TrainSound
-    var stream_player: AudioStreamPlayer2D = $AudioStreamPlayer3D
-    var stream_player_2: AudioStreamPlayer2D = $AudioStreamPlayer3D2
-    var engine_sound: LayeredSoundResource = sounds.get_engine_sounds()
-    var rpm_495 = UserSettings.get_maszyna_game_dir() + "sounds/" + str(engine_sound.sound_table[495])
-    var rpm_600 = UserSettings.get_maszyna_game_dir() + "sounds/" + str(engine_sound.sound_table[600])
-    stream_player.stream = sounds.get_audio_stream_for_file(rpm_495, true)
-    stream_player.play()
-    stream_player.volume_db = -80.0
-    stream_player_2.stream = sounds.get_audio_stream_for_file(rpm_600, true)
-    stream_player_2.play()
-    stream_player_2.volume_db = -80.0
+    engine_sounds = sounds.get_engine_sounds()
     
 func _process(delta: float) -> void:
-    var stream_player: AudioStreamPlayer2D = $AudioStreamPlayer3D
-    var stream_player_2: AudioStreamPlayer2D = $AudioStreamPlayer3D2
+    #TODO: Gather AudioStreamPlayer from LayeredSoundResource and make a copy of it as stream_plauyer_2
     var rpm = $HSlider.value
-    var next_rpm = $HSlider.max_value
-    var vol_1 = linear_to_db((next_rpm - rpm)/next_rpm)
-    var vol_2 = linear_to_db(rpm/next_rpm)
-    print("vol_1: %s, vol_2: %s" % [vol_1, vol_2])
-    stream_player.volume_db = vol_1
-    stream_player_2.volume_db = vol_2
-    
+    $Label.text = "RPM: %s" % rpm
+    _update_audio_streams_and_volumes(rpm, engine_sounds.sound_table, engine_sounds.sound_table.keys(), engine_sounds.cross_fade)
 
-func calc_crossfade(current, next, treshold):
-    if (treshold == 100 || treshold == 0):
-        return current
-    if (treshold > 100 || treshold < 0):
-        push_error("[TrainSound] Crossfade treshold must be within a range between 0 and 100")
-        return
-    return (next-current)+((next-current)*(treshold/100))
+func _update_audio_streams_and_volumes(param_value: float, audio_map: Dictionary, sorted_parameter_keys: Array, crossfade_treshold_percent: int):
+    if sorted_parameter_keys.size() == 0: return
+    var idx_min = -1
+    for i in range(sorted_parameter_keys.size() - 1, -1, -1):
+        if param_value >= sorted_parameter_keys[i]:
+            idx_min = i
+            break
+
+    if idx_min == -1:
+        idx_min = 0
+
+    var param_min = sorted_parameter_keys[idx_min]
+    var stream_min = sounds.get_audio_stream_for_file(UserSettings.get_maszyna_game_dir() + "sounds/" + str(audio_map[int(param_min)]), true)
+    var param_max: float
+    var stream_max = null # null means max rpm scope
+    var idx_max = idx_min + 1
+    if idx_max < sorted_parameter_keys.size():
+        param_max = sorted_parameter_keys[idx_max]
+        stream_max = sounds.get_audio_stream_for_file(UserSettings.get_maszyna_game_dir() + "sounds/" + str(audio_map[int(param_max)]), true)
+    else:
+        param_max = param_min + 1
+
+    var p_min: AudioStreamPlayer2D
+    var p_max: AudioStreamPlayer2D
+    if idx_min % 2 == 0:
+        p_min = player_1
+        p_max = player_2
+    else:
+        p_min = player_2
+        p_max = player_1
+
+    if p_min.stream != stream_min:
+        p_min.stream = stream_min
+        print_verbose("Assigned stream_min (%s) to %s" % [stream_min.resource_path.get_file(), p_min.name])
+        if not p_min.playing: p_min.play()
+
+    if stream_max != null:
+        if p_max.stream != stream_max:
+            p_max.stream = stream_max
+            print_verbose("Assigned stream_max (%s) to %s" % [stream_max.resource_path.get_file(), p_max.name])
+            if not p_max.playing: p_max.play()
+    else:
+        if p_max.stream != null:
+            p_max.stream = null
+            print_verbose("Cleared stream from %s (highest RPM range)" % p_max.name)
+
+    var progress: float = 0.0
+    if stream_max != null and param_max > param_min:
+        if (crossfade_treshold_percent > 100 || crossfade_treshold_percent < 0):
+            push_error("[TrainSound] Crossfade treshold must be within a range between 0 and 100")
+            return
+        var crossfade_start_rpm = param_min + (param_max - param_min) * (crossfade_treshold_percent / 100.0)
+        progress = clamp(inverse_lerp(param_min, param_max, param_value), 0.0, 1.0)
+        var vol_min_linear: float = 1.0 - progress
+        var vol_max_linear: float = progress
+        p_min.volume_db = linear_to_db(vol_min_linear)
+        p_max.volume_db = linear_to_db(vol_max_linear)
+    else:
+        p_min.volume_db = MAX_VOLUME_DB
+        p_max.volume_db = MIN_VOLUME_DB
+    #print("RPM: %.1f | Range: [%d, %s] | Idx: %d | Progress: %.2f | p_min(%s): %.1f dB | p_max(%s): %.1f dB" % [current_rpm, rpm_min, str(rpm_max) if stream_max else "inf", idx_min, progress, p_min.name, p_min.volume_db, p_max.name, p_max.volume_db])

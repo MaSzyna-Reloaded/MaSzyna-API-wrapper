ffb6bfc94adae13332aff13d1e7aeb44e5fbb19a
Karol Tomaszewski <karoltomaszewskimusic@gmail.com>
2026-04-21 19:33:45 +0200

    Enhance CMakeLists for MSVC and debug handling
    Add support for MSVC specific compile options and multi-config output directories.

--- src/core/GameLog.hpp
+++ src/core/GameLog.hpp
@@ -7,7 +7,7 @@
 #include <godot_cpp/variant/utility_functions.hpp>
 #include <map>
 
-#ifdef LIBMASZYNA_DEBUG_ENABLED
+#ifdef LIBMASZYNA_DEBUG
 #if defined(_MSC_VER)
 // MSVC doesn't require special handling.
 #define DEBUG(msg, ...)                                                                                                \

5eb178922b1180ee43753a6efdd27f7cc4e8935d
Marcin Nowak <marcin.j.nowak@gmail.com>
2026-04-23 12:01:30 +0200

    (#191) Remove ObservableValue

--- src/core/utils.hpp
+++ src/core/utils.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include <functional>
 #include <string>
 
 #define ENUM_WITH_COUNT(name, ...) enum class name { __VA_ARGS__, COUNT };
@@ -29,55 +28,4 @@ namespace libmaszyna::utils {
     }
 
     double random(double p_a, double p_b);
-
-    /**
-     * @brief A templated class that wraps a value and calls a callback function
-     * whenever the value is changed.
-     *
-     * @tparam T The type of the variable to be observed.
-     */
-    template<typename T>
-    class ObservableValue {
-        private:
-            T value;
-            std::function<void(const T&)> callback;
-
-        public:
-            /**
-             * ObservableValue watches any change to the variable it declares and calls the provided callback as it changes
-             * @param p_initial_value Initial value
-             * @param p_callback_fn Callback function taking no arguments
-             */
-            ObservableValue(const T& p_initial_value, std::function<void()> p_callback_fn)
-                : value(p_initial_value), callback([fn = std::move(p_callback_fn)](const T&) { if (fn) { fn(); } }) {}
-
-            /**
-             * ObservableValue watches any change to the variable it declares and calls the provided callback as it changes
-             * @param p_initial_value Initial value
-             * @param p_callback_fn Callback function taking the new value (const T&)
-             */
-            ObservableValue(const T& p_initial_value, std::function<void(const T&)> p_callback_fn)
-                : value(p_initial_value), callback(std::move(p_callback_fn)) {}
-
-            // Core logic: Overload assignment operator - calls callback with one argument (new value)
-            ObservableValue<T>& operator=(const T& p_new_value) {
-                if (value != p_new_value) {
-                    value = p_new_value;
-                    if (callback) {
-                        callback(p_new_value); // Call callback with the new value
-                    }
-                }
-                return *this;
-            }
-
-            // Implicit conversion operator for easy reading
-            explicit operator T() const {
-                return value;
-            }
-
-            // Explicit getter
-            const T& get() const {
-                return value;
-            }
-    };
 } // namespace libmaszyna::utils
--- src/models/e3d/instance/E3DModelInstance.hpp
+++ src/models/e3d/instance/E3DModelInstance.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include "./core/utils.hpp"
 #include "E3DModelInstanceManager.hpp"
 #include "macros.hpp"
 #include "models/e3d/E3DModel.hpp"
@@ -11,17 +10,6 @@ namespace godot {
     class E3DNodesInstancer; // forward declaration
     class E3DModelInstance: public VisualInstance3D {
         GDCLASS(E3DModelInstance, VisualInstance3D)
-        private:
-            void _reload();
-            void _deferred_reload();
-            void _flush_pending_model();
-            E3DModelInstanceManager *_get_manager() const;
-            bool _is_dirty = false;
-            bool _pending_model_scheduled = false;
-            Ref<Mutex> _mutex;
-            Ref<E3DModel> _pending_model;
-        protected:
-            static void _bind_methods();
         public:
             static const char *e3d_loaded_signal;
             enum Instancer {
@@ -30,19 +18,45 @@ namespace godot {
                 INSTANCER_EDITABLE_NODES,
             };
 
+        private:
+            void _reload();
+            void _request_reload();
+            void _deferred_reload();
+            void _flush_pending_model();
+            E3DModelInstanceManager *_get_manager() const;
+            bool _is_dirty = false;
+            bool _pending_model_scheduled = false;
+            Ref<Mutex> _mutex;
+            Ref<E3DModel> _pending_model;
+            String data_path;
+            String model_filename;
+            Array skins;
+            Array exclude_node_names;
+            Instancer instancer = INSTANCER_NODES;
+            AABB submodels_aabb;
+            bool editable_in_editor = false;
+        protected:
+            static void _bind_methods();
+        public:
             //@TODO: Maybe remove this?
             void _notification(int p_what);
             E3DModelInstance();
             ~E3DModelInstance() override;
             MAKE_MEMBER_GS(Vector3, default_aabb_size, Vector3(1, 1, 1))
-            // Observable values that trigger _reload() when changed
-            MAKE_MEMBER_OBSERVABLE_GS(String, data_path);
-            MAKE_MEMBER_OBSERVABLE_GS(String, model_filename);
-            MAKE_MEMBER_OBSERVABLE_GS(Array, skins);
-            MAKE_MEMBER_OBSERVABLE_GS(Array, exclude_node_names);
-            MAKE_MEMBER_OBSERVABLE_GS_NR(Instancer, instancer)
-            MAKE_MEMBER_OBSERVABLE_GS(AABB, submodels_aabb)
-            MAKE_MEMBER_OBSERVABLE_GS(bool, editable_in_editor)
+            String get_data_path() const;
+            void set_data_path(const String &p_value);
+            String get_model_filename() const;
+            void set_model_filename(const String &p_value);
+            Array get_skins() const;
+            void set_skins(const Array &p_value);
+            Array get_exclude_node_names() const;
+            void set_exclude_node_names(const Array &p_value);
+            Instancer get_instancer() const;
+            void set_instancer(Instancer p_value);
+            AABB get_submodels_aabb() const;
+            void set_submodels_aabb(const AABB &p_value);
+            bool get_editable_in_editor() const;
+            void set_editable_in_editor(const bool &p_value);
             
             void _instantiate_children(const Ref<E3DModel> &p_model);
     };

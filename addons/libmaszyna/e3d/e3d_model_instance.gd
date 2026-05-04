a8d5b7fc54260b5c97f26dd7cb1fdc271088093c
Marcin Nowak <marcin.j.nowak@gmail.com>
2026-05-04 03:00:52 +0200

    (#213) Reimplement optimized nodes instancer
    * implements E3DModelInstance optimized instancer
    * implements E3DModelTool.get_aabb() helper
    * adds E3DModelInstance.model property for setting E3DModel directly
    * moves ResourceFormatLoaders registration to libmaszyna_runtime.gd
      (this will be dropped after porting loaders to C++)
    
    Known limitations:
    * Optimized E3DModelInstances aren't pickable in the Godot Editor

--- addons/libmaszyna/e3d/e3d_model_instance.gd
+++ addons/libmaszyna/e3d/e3d_model_instance.gd
@@ -1,43 +1,70 @@
 @tool
 extends VisualInstance3D
+
+## Displays a MaSzyna E3D model in 3D space.
+##
+## [E3DModelInstance] loads an [E3DModel] and instantiates it using one of the
+## supported instancers. [code]NODES[/code] and [code]EDITABLE_NODES[/code]
+## build a regular node hierarchy. [code]OPTIMIZED[/code] renders through
+## [code]RenderingServer[/code] and does not create child mesh nodes.
+## If [member model] is set, it will be used to instnatiate. Otherwise the
+## [member data_path] and [member model_filename] will be used to load with [E3DModelManager].
 class_name E3DModelInstance
 
+## Emitted after the current model instance has been created.
 signal e3d_loaded
 
+## Selected instancing backend.
 enum Instancer {
-    OPTIMIZED,
-    NODES,
-    EDITABLE_NODES,
+    OPTIMIZED,  ## Renders using [code]RenderingServer[/code] without creating child mesh nodes.
+    NODES,  ## Creates a regular hierarchy of generated 3D nodes.
+    EDITABLE_NODES,  ## Creates generated 3D nodes intended to stay editable in the editor.
 }
 
-var _dirty: bool = true
+var _model: E3DModel
+var _dirty: bool = false
+var _current_instancer: E3DInstancer = null
+var _current_editable: bool = false
+
 var default_aabb_size: Vector3 = Vector3(1, 1, 1)
 
+
+## E3DModel to instantiate (leave empty, if you want to lazy load with [member model_filename])
+@export var model: E3DModel:
+    set(x):
+        if not x == model:
+            model = x
+            _dirty = true
+
+## Base MaSzyna data path used to resolve model files and materials.
 @export var data_path:String = "":
     set(x):
         if not x == data_path:
             data_path = x
             _dirty = true
 
+## Name of the [code].e3d[/code] model file to load (if [member model] is not used).
 @export var model_filename:String = "":
     set(x):
         if not x == model_filename:
             model_filename = x
             _dirty = true
 
+## Optional material skin overrides used by dynamic submodel materials.
 @export var skins:Array = []:
     set(x):
         if not x == skins:
             skins = x
             _dirty = true
 
+## Submodel names that should be skipped during instantiation.
 @export var exclude_node_names:Array = []:
     set(x):
         if not x == exclude_node_names:
             exclude_node_names = x
             _dirty = true
 
-# Probably instancer should be set project-wide
+## Selected instancing backend. The default is [code]NODES[/code].
 @export var instancer = Instancer.NODES:
     set(x):
         if not x == instancer:
@@ -45,6 +72,7 @@ var default_aabb_size: Vector3 = Vector3(1, 1, 1)
             _dirty = true
 
 var submodels_aabb:AABB = AABB()
+## When enabled, node-based instancing keeps generated children editable in the editor.
 var editable_in_editor:bool = false:
     set(x):
         if not editable_in_editor == x:
@@ -54,6 +82,15 @@ var editable_in_editor:bool = false:
 
 func _get_aabb() -> AABB:
     return submodels_aabb
+
+
+func _resolve_instancer() -> E3DInstancer:
+    match instancer:
+        Instancer.NODES, Instancer.EDITABLE_NODES:
+            return E3DNodesInstancer
+        Instancer.OPTIMIZED:
+            return E3DOptimizedInstancer
+    return null
 
 
 func _process(_delta: float) -> void:
@@ -67,23 +104,48 @@ func _process_dirty(_delta: float) -> void:
     reload()
 
 
-func reload():
-    if is_inside_tree() and model_filename and data_path:
-        if data_path and model_filename:
-            var _do_load = func():
-                var model = E3DModelManager.load_model(data_path, model_filename)
-                if model:
-                    match instancer:
-                        E3DModelInstance.Instancer.NODES:
-                            var _do_instantiate = func():
-                                E3DNodesInstancer.instantiate(model, self, editable_in_editor)
-                                e3d_loaded.emit()
-                            SceneryResourceLoader.schedule(
-                                "Creating model %s" % name, _do_instantiate
-                            )
-                        _:
-                            push_error("Selected instancer is not supported!")
-            SceneryResourceLoader.schedule("Loading %s" % name, _do_load)
+## Reloads the configured E3D model and recreates the current instance using the selected instancer.
+func reload() -> void:
+    if is_inside_tree() and (model or model_filename):
+        if _current_instancer:
+            _current_instancer.clear(self)
+        _current_instancer = _resolve_instancer()
+        var _do_load = func():
+            if model:
+                _model = model
+            else:
+                _model = E3DModelManager.load_model(data_path, model_filename)
+            if _model:
+                submodels_aabb = E3DModelTool.get_aabb(_model)
 
-func _ready():
+                _current_editable = editable_in_editor or instancer == Instancer.EDITABLE_NODES
+                if _current_instancer:
+                    _current_instancer.clear(self)
+                    _current_instancer.instantiate(self, _model, _current_editable)
+                    e3d_loaded.emit()
+                else:
+                    push_error("Selected instancer is not supported!")
+
+        SceneryResourceLoader.schedule("Loading %s" % name, _do_load)
+
+
+func _notification(what: int) -> void:
+    match what:
+        NOTIFICATION_TRANSFORM_CHANGED, NOTIFICATION_VISIBILITY_CHANGED:
+            if _current_instancer:
+                _current_instancer.sync(self)
+
+
+func _ready() -> void:
+    set_notify_transform(true)
     reload()
+    
+    
+func _enter_tree() -> void:
+    if _model and _current_instancer:
+        _current_instancer.instantiate(self, _model, _current_editable)
+
+
+func _exit_tree() -> void:
+    if _current_instancer:
+        _current_instancer.clear(self)

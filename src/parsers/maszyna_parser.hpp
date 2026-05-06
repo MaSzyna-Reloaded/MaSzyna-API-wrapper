70ae07240d3e55c906d17b9c782392d590f50b39
Marcin Nowak <marcin.j.nowak@gmail.com>
2026-05-06 22:06:20 +0200

    (#242) Improve Maszyna Parser
    * replace std::vector with TypedArray
    * add unregister_handler()
    * fix incorrect parsing of inline commets something // coment
    * introduce default stop chars and make stops optional in methods
      like get_tokens_until() or next_token()

--- src/parsers/maszyna_parser.hpp
+++ src/parsers/maszyna_parser.hpp
@@ -1,42 +1,47 @@
 #pragma once
 
-#include <godot_cpp/classes/object.hpp>
+#include <godot_cpp/classes/ref_counted.hpp>
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/variant/dictionary.hpp>
 #include <godot_cpp/variant/packed_byte_array.hpp>
 #include <godot_cpp/variant/string.hpp>
+#include <godot_cpp/variant/typed_array.hpp>
 #include <godot_cpp/variant/vector3.hpp>
 #include <vector>
 
 namespace godot {
-    class MaszynaParser : public Object {
-            GDCLASS(MaszynaParser, Object);
+    class MaszynaParser : public RefCounted {
+            GDCLASS(MaszynaParser, RefCounted);
 
         private:
             PackedByteArray buffer;
             Dictionary handlers;
             int cursor = 0;
             int length = 0;
-            std::vector<Dictionary> meta;
+            TypedArray<Dictionary> meta;
+            Array default_stop_chars;
             Dictionary parameters;
+            Array get_stops(const Array &p_stops) const;
 
         protected:
             static void _bind_methods();
 
         public:
             MaszynaParser();
-            void initialize(const PackedByteArray &p_buffer);
+            void initialize(const PackedByteArray &p_buffer, const Array &p_default_stop_chars);
             // void _create_instance(const PackedByteArray &buffer);
             int get8();
             String get_line();
             bool eof_reached() const;
             void register_handler(const String &p_token, const Callable &p_callback);
+            void unregister_handler(const String &p_token);
+            void set_parameters(const Dictionary &p_parameters);
             bool as_bool(const String &p_token);
             Vector3 as_vector3(const Array &p_tokens);
-            Array get_tokens(int p_num, const Array &p_stop);
-            String next_token(const Array &p_stop);
-            Vector3 next_vector3(const Array &p_stop);
-            Array get_tokens_until(const String &p_token, const Array &p_stop);
+            Array get_tokens(int p_num, const Array &p_stops = Array());
+            String next_token(const Array &p_stops = Array());
+            Vector3 next_vector3(const Array &p_stops = Array());
+            Array get_tokens_until(const String &p_token, const Array &p_stops = Array());
             Array parse();
             Dictionary get_parsed_metadata();
             void push_metadata();

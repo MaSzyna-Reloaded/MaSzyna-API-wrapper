6cf286522a9f5e6d19522318909e9583d2052bba
Marcin Nowak <marcin.j.nowak@gmail.com>
2026-05-17 15:55:45 +0200

    (#250) Improve node bank drag&drop preview

--- addons/libmaszyna/editor/nodebank/plugin.gd
+++ addons/libmaszyna/editor/nodebank/plugin.gd
@@ -1,25 +1,29 @@
 @tool
 extends EditorPlugin
 
-var _dragging := false
+var _dragging: bool = false
 var _drag_data: NodebankGridItem
 var _drag_camera: Camera3D
 var _drag_position: Vector2 = Vector2.ZERO
-
+var _drag_preview_instance: E3DModelInstance
 
 var nodebank_panel_scene = preload("./nodebank_panel.tscn")
 var nodebank_panel_instance
 var nodebank_bottom_button
 
-
 func drag_start(item_data: NodebankGridItem) -> void:
+    _dragging = true
     if item_data:
-        _dragging = true
         _drag_data = item_data
-        _drag_camera = null
-        _drag_position = Vector2.ZERO
+        var scene_root: Node = EditorInterface.get_edited_scene_root()
+        _drag_preview_instance = _drag_data.model.duplicate() as E3DModelInstance
+        scene_root.add_child(_drag_preview_instance, false, Node.InternalMode.INTERNAL_MODE_BACK)
+        _drag_preview_instance.owner = null
 
 func drag_reset() -> void:
+    if _drag_preview_instance:
+        _drag_preview_instance.queue_free()
+    _drag_preview_instance = null
     _drag_data = null
     _drag_camera = null
     _dragging = false
@@ -51,9 +55,6 @@ func drag_end() -> void:
     EditorInterface.get_selection().clear()
     EditorInterface.get_selection().add_node(instance)
 
-func _ready() -> void:
-    set_process(true)
-
 func _enter_tree() -> void:
     set_input_event_forwarding_always_enabled()
         
@@ -69,36 +70,34 @@ func _exit_tree() -> void:
     nodebank_panel_instance.free()
     nodebank_panel_instance = null
 
-func _process(_delta: float) -> void:
-    if not _dragging or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-        return
-
-    # handle drop
-    if not is_instance_valid(_drag_camera):
-        var editor_viewport: SubViewport = EditorInterface.get_editor_viewport_3d(0)
-        if editor_viewport:
-            var editor_position: Vector2 = editor_viewport.get_mouse_position()
-            if editor_viewport.get_visible_rect().has_point(editor_position):
-                _drag_camera = editor_viewport.get_camera_3d()
-                _drag_position = editor_position
-
-    if _drag_camera:
-        drag_end()
-        drag_reset()
-
-func _forward_3d_gui_input(viewport_camera: Camera3D, event: InputEvent) -> int:
-    if not _dragging:
-        return AFTER_GUI_INPUT_PASS
-
-    if not _drag_data:
-        return AFTER_GUI_INPUT_PASS
-
-    if event is InputEventMouseMotion:
-        var mouse_motion: InputEventMouseMotion = event as InputEventMouseMotion
-        _drag_camera = viewport_camera
-        _drag_position = mouse_motion.position
-
-    return AFTER_GUI_INPUT_PASS
+func _process(_detla: float) -> void:
+    if _dragging:
+        var viewport = EditorInterface.get_editor_viewport_3d(0)
+        var position = viewport.get_mouse_position()
+        if viewport.get_visible_rect().has_point(position):
+            # mouse is over editor viewport
+            _drag_position = position
+            _drag_camera = viewport.get_camera_3d()
+        else:
+            # mouse is outside editor viewport
+            _drag_camera = null
+            
+        if _drag_preview_instance:
+            # handle 3d preview
+            _drag_preview_instance.visible = not _drag_camera == null
+            if _drag_camera:        
+                var scene_root: Node = EditorInterface.get_edited_scene_root()
+                var world_position: Vector3 = _get_drop_world_position(_drag_camera, _drag_position)
+                _drag_preview_instance.position = scene_root.global_transform.affine_inverse() * world_position
+            
+        # handle drop
+        var mouse_pressed = Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
+        if not mouse_pressed:
+            if _drag_camera:
+                # mouse is over the editor viewport
+                drag_end()
+            # mouse was released, so stop dragging
+            drag_reset()
 
 func _get_drop_world_position(viewport_camera: Camera3D, screen_position: Vector2) -> Vector3:
     var ray_origin: Vector3 = viewport_camera.project_ray_origin(screen_position)
